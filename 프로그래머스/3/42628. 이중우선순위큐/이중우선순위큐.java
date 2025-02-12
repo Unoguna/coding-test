4b48b2923e4d4305046b45fc99bd3fc5270525c0
Unoguna <82808715+Unoguna@users.noreply.github.com>
2025-02-12 20:28:34 +0900

    [level 3] Title: 이중우선순위큐, Time: 53.85 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -7,8 +7,6 @@ class Solution {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
         
         for(int i=0; i<operations.length; i++){
             char op = operations[i].charAt(0);
@@ -17,29 +15,14 @@ class Solution {
             if(op == 'I'){
                 minHeap.add(num);
                 maxHeap.add(num);
-                map.put(num, map.getOrDefault(num, 0) + 1);
             }
             else if(num == 1 && !maxHeap.isEmpty()){
-                while(!maxHeap.isEmpty() && map.get(maxHeap.peek()) == 0) //이미 없어짐
-                    maxHeap.poll();
-                if(!maxHeap.isEmpty()){
-                    map.put(maxHeap.peek(), map.get(maxHeap.peek()) - 1);
-                    maxHeap.poll();
-                }
+                minHeap.remove(maxHeap.poll());
             }
             else if(!minHeap.isEmpty()){
-                while(!minHeap.isEmpty() && map.get(minHeap.peek()) == 0) //이미 없어짐
-                    minHeap.poll();
-                if(!minHeap.isEmpty()){
-                    map.put(minHeap.peek(), map.get(minHeap.peek()) - 1);
-                    minHeap.poll();
-                }
+                maxHeap.remove(minHeap.poll());
             }   
         }
-        while(!maxHeap.isEmpty() && map.get(maxHeap.peek()) == 0) //이미 없어짐
-            maxHeap.poll();
-        while(!minHeap.isEmpty() && map.get(minHeap.peek()) == 0) //이미 없어짐
-            minHeap.poll();
         
         
         if(!maxHeap.isEmpty()){
