6c36265750cdf33510cd0be1e4d4b9ba3c75a7d8
Unoguna <82808715+Unoguna@users.noreply.github.com>
2025-06-30 17:57:16 +0900

    [Bronze IV] Title: 영수증, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        
+        int sum = 0;
+        
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sum += a * b;
+        }
+        if(X == sum) System.out.println("Yes");
+        else System.out.println("No");
+        bw.flush();
+	}
+}
