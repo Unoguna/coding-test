37e8ef538fd9921d4a261b149c5e721bd922b996
Unoguna <82808715+Unoguna@users.noreply.github.com>
2025-06-30 12:56:48 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 23.74 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int r1, int r2) {
+        long answer = 0;
+        
+        answer += (long)(r2 - r1 + 1) * 4;
+        
+        for(int i=1; i<r2; i++){
+            
+            long tmp1 = (long)Math.pow(r2,2);
+            long tmp2 = (long)Math.pow(i,2);
+            
+            long top = (long)Math.floor(Math.sqrt(tmp1 - tmp2));
+            answer += top * 4;
+        }
+        
+        for(int i=1; i<r1; i++){
+            long tmp1 = (long)Math.pow(r1,2);
+            long tmp2 = (long)Math.pow(i,2);
+            
+            long top = (long)Math.ceil(Math.sqrt(tmp1 - tmp2)) - 1;
+            answer -= top * 4;
+        }
+
+        return answer;
+    }
+}
